f3d3c0fa11187cb8009bfe2bbf70b1b8a8ea76da
Vijay-Shankaran <104640280+Vijay-Shankaran@users.noreply.github.com>
2024-01-12 16:22:12 -0500

    Update controller.py

--- embodiments/arduino/radio_controller/pyserial_experiment/controller.py
+++ embodiments/arduino/radio_controller/pyserial_experiment/controller.py
@@ -3,40 +3,33 @@ import time
 import json
 from configuration import *
 from feagi_agent import feagi_interface as feagi
+from feagi_agent import retina as retina
 from feagi_agent import pns_gateway as pns
 from feagi_agent.version import __version__
 from feagi_agent import actuators
+import requests
+import cv2
+import threading
+from datetime import datetime
+
+camera_data = {"vision": {}}
 
 
-def initialization_port(port):
-    return serial.Serial(port,
-                         baudrate=9600,
-                         timeout=2.5,
-                         parity=serial.PARITY_NONE,
-                         bytesize=serial.EIGHTBITS,
-                         stopbits=serial.STOPBITS_ONE)
-
-
-def convert_dict_to_json(data):
-    data = json.dumps(data)
-    return data
-
-
-def send_serial(ser, data):
-    ser.write(data.encode('ascii'))
+def process_video(path, capabilities):
+    cam = cv2.VideoCapture(0)
+    while True:
+        check, pixels = cam.read()
+        camera_data["vision"] = pixels
+    cam.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
+    threading.Thread(target=process_video, args=(capabilities['camera']['video_device_index'],
+                                                 capabilities), daemon=True).start()
     print("Ready...")
-    ser = initialization_port(capabilities['arduino']['port'])
+    ser = actuators.initialization_port(capabilities['arduino']['port'])
     feagi_flag = False
-    print("Waiting on FEAGI...")
-    while not feagi_flag:
-        feagi_flag = feagi.is_FEAGI_reachable(os.environ.get('FEAGI_HOST_INTERNAL',
-                                                             "127.0.0.1"), int(os.environ.get(
-            'FEAGI_OPU_PORT', "3000")))
-        sleep(2)
-    print("DONE")
     previous_data_frame = {}
     runtime_data = {"cortical_data": {}, "current_burst_id": None,
                     "stimulation_period": 0.01, "feagi_state": None,
@@ -48,6 +41,12 @@ if __name__ == "__main__":
                                __version__)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     msg_counter = runtime_data["feagi_state"]['burst_counter']
+    rgb = dict()
+    rgb['camera'] = dict()
+    response = requests.get(api_address + '/v1/feagi/genome/cortical_area/geometry')
+    capabilities['camera']['size_list'] = retina.obtain_cortical_vision_size(capabilities['camera']["index"], response)
+    previous_frame_data = {}
+    raw_frame = []
     device_list = pns.generate_OPU_list(capabilities)
 
     # Experimenting...delete this
@@ -56,20 +55,61 @@ if __name__ == "__main__":
     data[2] = 1
     # Ends
 
-    # To give ardiuno some time to open port. It's required
+    # To give ardiuno some time to open serial. It's required
     time.sleep(5)
+    cam = cv2.VideoCapture(0)
     while True:
         message_from_feagi = pns.signals_from_feagi(feagi_opu_channel)
 
-        # Fetch data such as motor, servo, etc and pass to a function (you make ur own action. 
+        # Fetch data such as motor, servo, etc and pass to a function (you make ur own action.
         if message_from_feagi is not None:
             obtained_signals = pns.obtain_opu_data(device_list, message_from_feagi)
             new_dict = dict()
-            if 'motor' in obtained_signals:
-                if obtained_signals['motor']:
-                    for x in obtained_signals['motor']:
-                        if x in [0, 1, 2, 3]:
-                            new_dict[x] = obtained_signals['motor'][x]
-                    json_data = convert_dict_to_json(new_dict) #convert dict to JSON
+            if 'servo' in obtained_signals:
+                if obtained_signals['servo']:
+                    print("recieved")
+                    one_hot_encoded = [0, 0, 0, 0, 0, 0, 0, 0]
+                    for x in obtained_signals['servo']:
+                        # if x in [0, 1, 2, 3]:
+                            # new_dict[x] = obtained_signals['servo'][x]
+                            # new_data = actuators.keep_boundaries([0, 180], obtained_signals['servo'][x])
+                        one_hot_encoded[x] = obtained_signals['servo'][x]
+                    # no judge
+
+                    data_str = ','.join(map(str, one_hot_encoded)) + '\n'
+                    print(data_str)
+                    # json_data = actuators.convert_dict_to_json(new_dict)
+                    # print (json_data)
+
                     if ser.isOpen():
-                        send_serial(ser, json_data)
+                        ser.write(data_str.encode())
+                        print("sent")
+        if camera_data['vision'] is not None:
+            raw_frame = camera_data['vision']
+        if capabilities['camera']['blink'] != []:
+            raw_frame = capabilities['camera']['blink']
+        previous_frame_data, rgb = retina.detect_change_edge(raw_frame, capabilities,
+                                                             capabilities['camera']["index"],
+                                                             capabilities['camera']['size_list'],
+                                                             previous_frame_data, rgb)
+        # capabilities['camera']['effect'].clear()
+        capabilities['camera']['blink'] = []
+        capabilities, feagi_settings['feagi_burst_speed'] = \
+            retina.vision_progress(capabilities, feagi_opu_channel, api_address, feagi_settings,
+                                   raw_frame)
+
+        message_to_feagi = pns.generate_feagi_data(rgb, msg_counter, datetime.now(),
+                                                   message_to_feagi)
+
+        sleep(0.01)
+        pns.signals_to_feagi(message_to_feagi, feagi_ipu_channel, agent_settings)
+        # print('sent')
+        message_to_feagi.clear()
+        for i in rgb['camera']:
+            rgb['camera'][i].clear()
+            # actuators.send_serial(ser, json_data)
+            # time.sleep(1)
+            # ser.readline()
+            # time.sleep(2)
+
+
