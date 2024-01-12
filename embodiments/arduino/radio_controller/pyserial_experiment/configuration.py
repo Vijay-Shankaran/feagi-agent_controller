d1d862a28f4d30d2db2ccfc11feb238601863d20
Vijay-Shankaran <104640280+Vijay-Shankaran@users.noreply.github.com>
2024-01-12 16:21:52 -0500

    Update configuration.py

--- embodiments/arduino/radio_controller/pyserial_experiment/configuration.py
+++ embodiments/arduino/radio_controller/pyserial_experiment/configuration.py
@@ -32,18 +32,45 @@ agent_settings = {
     "agent_type": "embodiment",
     'TTL': 2,
     'last_message': 0,
-    'godot_websocket_ip': "0.0.0.0",
-    'godot_websocket_port': os.environ.get('WS_WEBCAM_PORT', "9051"),
     'compression': True
 }
 
 capabilities = {
     "arduino": {
-        "port": "COM5"
+        "port": "/dev/ttyUSB1"
     },
-    "motor": {
+    "servo": {
         "type": "opu"
     },
+    "camera": {
+        "type": "ipu",
+        "disabled": False,
+        "index": "00",
+        "threshold_default": [10, 255, 130, 51], # min #1, max #1, min #2, max #2,
+        "threshold_range": [1, 255],
+        "threshold_type": {},
+        "threshold_name": 0,
+        "central_vision_allocation_percentage": [80, 60],
+        "central_vision_resolution": [64, 64],
+        "peripheral_vision_resolution": [8, 8],
+        "resolution_presets": [[8, 8], [16, 16], [32, 32], [64, 64], [80, 60], [128, 128], [160, 120],
+                               [256, 256], [320, 240], [400, 400], [480, 320], [500, 500], [800, 800], [1024, 900]],
+        "previous_data": {},
+        "video_device_index": 0,
+        "image": "",
+        "video_loop": False,
+        "mirror": False,
+        "monitor": 0,
+        "blink": [],
+        "gaze_control": {0: 1, 1: 99},
+        "pupil_control": {0: 1, 1: 99},
+        "vision_range": [1, 99],
+        "size_list": [],
+        "effect": {},
+        "enhancement": {},
+        "vision_tuner_range": [0, 255]
+    }
 }
+
 
 message_to_feagi = {"data": {}}
